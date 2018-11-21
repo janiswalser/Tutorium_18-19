791966fe0e1739dbf45cbb3c795ad7c69111c27e
janis <janis.walser@icloud.com>
2018-11-21 17:48:40 +0100

    Basics

---
+++ 01_Basics/main.js
@@ -0,0 +1,82 @@
+//Variables
+
+var example1 = 7;
+let example2 = 7.77;
+
+console.log(example1);
+console.log(example2);
+
+
+
+// -------------------------------------------------------------------------------
+
+//Strings
+
+var name = "Dylan";
+let name2 = "Max";
+
+
+console.log(name);
+
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+
+
+// -------------------------------------------------------------------------------
+
+//Numbers challenge
+
+let example3 = parseInt("33 World 22");
+let example4 = parseFloat('44 Dylan 33');
+let example5 = 55.3333.toFixed(0);
+let example6 = 200.0.toFixed(2);
+
+console.log(example3);
+console.log(example4);
+console.log(example5);
+console.log(example6);
+
+
+
+// -------------------------------------------------------------------------------
+
+// Boolean
+
+let example7 = true;
+let example8 = false;
+
+console.log(example7);
+console.log(example8);
+
+
+// -------------------------------------------------------------------------------
+
+// Arrays
+
+let example9 = ['programming', 'design', 'art'];
+
+console.log(example9);
+console.log(example9[0]);
+
+// -------------------------------------------------------------------------------
+
+// push Arrays
+
+let example10 = ["A.i.", "Blockchain", "Design Thinking", "Art",'programming', 'design', 'art'];
+let example11 = "shit";
+example10.push(example11);
+
+console.log(example11);
+
+// -------------------------------------------------------------------------------
+
+
+//Operators
+
+let example13 = 10;
+let example14 = 15;
+
+console.log(example13 > example14);
+console.log(example13 != example14);
+console.log(example13 == example13);
+
